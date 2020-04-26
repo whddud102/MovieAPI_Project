8ca29b60ae39c101d86ff866be475cd6a30ec817
JongYoung <JongYoung@14.40.51.99>
2020-04-26 16:08:06 +0900

    코드 추가
    자바 기반 설정 파일 추가

---
+++ MovieAPI/src/main/java/com/jy/config/RootConfig.java
@@ -0,0 +1,8 @@
+package com.jy.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig{
+
+}
--- MovieAPI/src/main/java/com/jy/controller/HomeController.java
+++ MovieAPI/src/main/java/com/jy/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.jy.movie;
+package com.jy.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -25,6 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
