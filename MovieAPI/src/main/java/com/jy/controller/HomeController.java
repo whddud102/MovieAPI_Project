7c7f41936421bfc1b71c0d0de254f836e0c20be1
JongYoung <JongYoung@14.40.51.99>
2020-04-27 23:46:22 +0900

    리팩토링
    - HomeController에서 service 계층을 이용하도록 수정

--- MovieAPI/src/main/java/com/jy/controller/HomeController.java
+++ MovieAPI/src/main/java/com/jy/controller/HomeController.java
@@ -1,10 +1,16 @@
 package com.jy.controller;
 
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.jy.domain.API_ResponseVO;
+import com.jy.domain.MovieVO;
+import com.jy.service.MovieService;
 import com.jy.utils.MovieAPI_Util;
 
 import lombok.AllArgsConstructor;
@@ -15,15 +21,17 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class HomeController {
 	
-	private MovieAPI_Util movieAPI_Util;
+	private MovieService movieService;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * @throws Exception 
 	 */
 	@GetMapping("/")
-	public String home(Model model) {
-		log.info("restTemplate 주입 확인 : " + movieAPI_Util);
+	public String home(Model model) throws Exception {
+		List<MovieVO> items =  movieService.getItems("그늘");
 		
+		model.addAttribute("items", items);
 		return "home";
 	}
 	
