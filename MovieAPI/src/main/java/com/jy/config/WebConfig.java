42e04e89b2365e85f7affcb344a832801998d3e2
JongYoung <JongYoung@14.40.51.99>
2020-04-28 17:09:56 +0900

    리팩토링
    - WebConfig.java : 한글 처리 필터 등록

--- MovieAPI/src/main/java/com/jy/config/WebConfig.java
+++ MovieAPI/src/main/java/com/jy/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.jy.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
@@ -17,6 +20,16 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] {"/"};
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter
+		.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] {characterEncodingFilter};
 	}
 
 }
